cc5c2440a27eb02b192344e776024021b72994cc
danielmurray <daniel.phillip.murray@gmail.com>
2015-07-14 16:16:49 -0500

    complete quote UI

--- js/uirplprint.js
+++ js/uirplprint.js
@@ -4,8 +4,6 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-
-
 (function($) {
     "use strict"; // Start of use strict
 
@@ -39,29 +37,108 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
-    var appendPart = function(){
-		_.templateSettings.variable = "rc";
+    var Receipt = function() {
+    	this.startUpCost = 5;
+    	this._partCount = 0
+    	this.parts = []
 
-	    // Grab the HTML out of our template tag and pre-compile it.
-	    var template = _.template(
-	        $( "script#receipt-item" ).html()
+	    this.printers = {
+	    	"sls":{
+	    		id: "sls",
+	    		name: "Selective Laser Sintering",
+	    		cost: 0.15
+	    	},
+	    	"pj":{
+	    		id: "pj",
+	    		name: "PolyJet",
+	    		cost: 0.25
+	    	},
+	    	"sla":{
+	    		id: "sla",
+	    		name: "Stereo Lithography Apparatus",
+	    		cost: 0.35
+	    	},
+	    	"fdm":{
+	    		id: "fdm",
+	    		name: "Fused Deposition Modeling",
+	    		cost: 0.45
+	    	},
+	    };
+
+	    this.template =  _.template(
+	        $( "#receipt-template" ).html()
 	    );
 
-	    // Render the underscore template and inject it
-	    // in our current DOM.
-	    $( ".receipt-items" ).append(
-	        template()
-	    );
+	    this.addPart = function() {
+	    	this.parts.push({
+	    		id: "part-"+this._partCount,
+		    	vol: 0,
+		    	printer: "sls"    	
+		    });
 
-	    $(".remove-part").click(function(e){
-			$(event.currentTarget).closest(".receipt-item-wrapper").remove()
-		})
+    		this._partCount++; 
+	    };
+
+	    this.removePart = function(partID) {			
+			for (var i = 0; i < this.parts.length; i++){
+				if (this.parts[i].id == partID) { 
+			        this.parts.splice(i, 1);
+			        break;
+			    }
+			} 
+	    };
+
+	     this.updatePart = function(update) {
+			var result = $.grep(this.parts, function(e){ return e.id == update.id; })[0];
+			$.extend(result, result, update)
+			console.log(result)
+	    };
+
+	    this.render = function() {
+		    $( "#quoteReceipt" ).html(
+		        this.template({
+		        	parts: this.parts,
+		        	printers: this.printers,
+		        	startup: this.startUpCost
+		        })
+		    );
+
+		    $(".add-part").click(function(){
+				receipt.addPart()
+				receipt.render();
+			})
+
+		    $(".remove-part").click(function(e){
+				var partID = $(event.currentTarget).closest(".receipt-item")[0].id;
+				receipt.removePart(partID)
+				receipt.render()
+			})
+
+			$(".form-control.printer").change(function(e){
+				var part = $(event.currentTarget).closest(".receipt-item")[0];
+				var optionSelected = $("option:selected", this)[0];
+				receipt.updatePart({
+					id: part.id,
+					printer: optionSelected.value
+				})
+				receipt.render()
+			})
+
+			$(".form-control.volume").change(function(e){
+				var part = $(event.currentTarget).closest(".receipt-item")[0];
+				var volume = $(event.currentTarget)[0];
+				receipt.updatePart({
+					id: part.id,
+					vol: volume.value
+				})
+				receipt.render()
+			})
+	    };
+
+	    this.addPart()
 	}
 
-	appendPart();
-
-	$(".add-part").click(function(){
-		appendPart();
-	})
+	var receipt = new Receipt();
+	receipt.render();
 
 })(jQuery); // End of use strict
